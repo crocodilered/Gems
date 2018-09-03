415c38129f5820940b464addb3d060d71c30352e
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-09-03 17:47:59 +0300

    Cell size setup bug fixed.

--- src/classes/View.js
+++ src/classes/View.js
@@ -8,16 +8,19 @@ class View {
     this.rootElemId = rootElemId
     this.rootElem = document.getElementById(this.rootElemId)
     this.swipeCallback = null
+    this.cellHeight = null
   }
 
   render (model) {
     this.rootElem.innerHTML = this.html(model)
     let tdElems = this.rootElem.getElementsByTagName('td')
+    if (!this.cellHeight) this.cellHeight = tdElems[0].clientWidth
     if (this.swipeCallback) {
       for (let i = 0; i < tdElems.length; i++) {
         /* eslint-disable no-new */
         new SwipeDetector(tdElems[i], (elem, direction) => this.swipeCallback(elem, direction))
-        tdElems[i].style.height = `${tdElems[i].clientWidth}px`
+        
+        tdElems[i].style.height = `${this.cellHeight}px`
       }
     }
   }
