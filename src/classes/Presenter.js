2d91ed25c8c4ce628be2970add8b5789db5722d2
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-09-03 17:37:31 +0300

    Row moving after gem moved implemented.

--- src/classes/Presenter.js
+++ src/classes/Presenter.js
@@ -15,34 +15,67 @@ class Presenter {
   swipeCallback (elem, direction) {
     let x = parseInt(elem.getAttribute('data-x'))
     let y = parseInt(elem.getAttribute('data-y'))
-    let gemMoved
-    if (direction === 'l' && x > 0) {
-      gemMoved = this.moveGem(x, y, this.model.get(x - 1, y))
-    }
-    if (direction === 'u' && y > 0) {
-      gemMoved = this.moveGem(x, y, this.model.get(x, y - 1))
-    }
-    if (direction === 'r' && x < this.model.size.width - 1) {
-      gemMoved = this.moveGem(x, y, this.model.get(x + 1, y))
-    }
-    if (direction === 'd' && y < this.model.size.height - 1) {
-      gemMoved = this.moveGem(x, y, this.model.get(x, y + 1))
-    }
-    if (gemMoved) this.view.render(this.model)
+    if (this.moveGem(x, y, direction)) this.view.render(this.model)
   }
 
-  moveGem (x, y, gem2) {
-    let gem1 = this.model.get(x, y)
-    if (gem1 && gem2 && gem1.color === gem2.color) {
-      if (gem1.weight > gem2.weight) {
-        gem2.weight = gem1.weight - gem2.weight
-      } else {
-        gem2.weight += gem1.weight
-      }
-      this.model.clear(x, y)
-      return true
+  moveGem (x1, y1, direction) {
+    let x2 = x1
+    let y2 = y1
+    
+    if (direction === 'l' && x1 > 0) x2 = x1 - 1
+    if (direction === 'u' && y1 > 0) y2 = y1 - 1
+    if (direction === 'r' && x1 < this.model.size.width - 1) x2 = x1 + 1
+    if (direction === 'd' && y1 < this.model.size.height - 1) y2 = y1 + 1
+    
+    // Test if x2 or y2 changed
+    if (x1 === x2 && y1 === y2) return false
+    
+    let gem1 = this.model.get(x1, y1)
+    let gem2 = this.model.get(x2, y2)
+
+    // Test if merging gems has same color
+    if (gem1 && gem2 && gem1.color !== gem2.color) return false
+
+    // Everythin is okay, merge gems
+    gem2.weight = (gem1.weight > gem2.weight)
+      ? gem1.weight - gem2.weight
+      : gem2.weight + gem1.weight
+
+    // Move row
+    if (direction === 'l') this.moveRowLeft(x1, y1)
+    if (direction === 'u') this.moveRowUp(x1, y1)
+    if (direction === 'r') this.moveRowRight(x1, y1)
+    if (direction === 'd') this.moveRowDown(x1, y1)
+
+    return true
+  }
+
+  moveRowUp (pointX, pointY) {
+    for (let y = pointY; y < this.model.size.height - 1; y++) {
+      this.model.set(pointX, y, this.model.get(pointX, y + 1))
     }
-    return false
+    this.model.clear(pointX, this.model.size.height - 1)
+  }
+
+  moveRowDown (pointX, pointY) {
+    for (let y = pointY; y > 0; y--) {
+      this.model.set(pointX, y, this.model.get(pointX, y - 1))
+    }
+    this.model.clear(pointX, 0)
+  }
+
+  moveRowLeft (pointX, pointY) {
+    for (let x = pointX; x < this.model.size.height - 1; x++) {
+      this.model.set(x, pointY, this.model.get(x + 1, pointY))
+    }
+    this.model.clear(this.model.size.width - 1, pointY)
+  }
+
+  moveRowRight (pointX, pointY) {
+    for (let x = pointX; x > 0; x--) {
+      this.model.set(x, pointY, this.model.get(x - 1, pointY))
+    }
+    this.model.clear(0, pointY)
   }
 
 }
