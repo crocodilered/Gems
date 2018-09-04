6791c7adc62b0e4cf2211f86ec4b63c4044852bd
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-09-04 18:13:12 +0300

    Empty cells collapsing bug fixed.

--- src/classes/View.js
+++ src/classes/View.js
@@ -8,19 +8,20 @@ class View {
     this.rootElemId = rootElemId
     this.rootElem = document.getElementById(this.rootElemId)
     this.swipeCallback = null
-    this.cellHeight = null
+    this.cellDimention = null
   }
 
   render (model) {
     this.rootElem.innerHTML = this.html(model)
     let tdElems = this.rootElem.getElementsByTagName('td')
-    if (!this.cellHeight) this.cellHeight = tdElems[0].clientWidth
+    if (!this.cellDimention) this.cellDimention = tdElems[0].clientWidth
     if (this.swipeCallback) {
       for (let i = 0; i < tdElems.length; i++) {
         /* eslint-disable no-new */
         new SwipeDetector(tdElems[i], (elem, direction) => this.swipeCallback(elem, direction))
         
-        tdElems[i].style.height = `${this.cellHeight}px`
+        tdElems[i].style.width = `${this.cellDimention}px`
+        tdElems[i].style.height = `${this.cellDimention}px`
       }
     }
   }
