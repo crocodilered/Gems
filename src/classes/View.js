f78325031b75135409cafb2326b9bf52ebf86a9a
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-09-05 11:52:36 +0300

    Rendered html changed to Flex-model.

--- src/classes/View.js
+++ src/classes/View.js
@@ -8,41 +8,48 @@ class View {
     this.rootElemId = rootElemId
     this.rootElem = document.getElementById(this.rootElemId)
     this.swipeCallback = null
-    this.cellDimention = null
+    this.cellSize = null // Size of cell
+  }
+
+  // run once before 1st render
+  preRender (model) {
+    this.cellSize = screen.width / model.size.width
+    this.rootElem.style.top = ((screen.height - screen.width) / 2) + 'px'
   }
 
   render (model) {
     this.rootElem.innerHTML = this.html(model)
-    let tdElems = this.rootElem.getElementsByTagName('td')
-    if (!this.cellDimention) this.cellDimention = tdElems[0].clientWidth
+    const cells = this.rootElem.getElementsByTagName('div')
     if (this.swipeCallback) {
-      for (let i = 0; i < tdElems.length; i++) {
+      for (let i = 0; i < cells.length; i++) {
         /* eslint-disable no-new */
-        new SwipeDetector(tdElems[i], (elem, direction) => this.swipeCallback(elem, direction))
-        // Set size of cell
-        tdElems[i].style.width = `${this.cellDimention}px`
-        tdElems[i].style.height = `${this.cellDimention}px`
+        new SwipeDetector(cells[i], (elem, direction) => this.swipeCallback(elem, direction))
       }
     }
   }
 
   html (model) {
     let html = ''
-    html += '<table>'
+    if (!this.cellSize) this.preRender(model)
     for (let y = 0; y < model.size.height; y++) {
-      html += '<tr>'
       for (let x = 0; x < model.size.width; x++) {
-        const gem = model.get(x, y)
-        if (gem) {
-          let bgSize = 10 + gem.weight * 5
-          html += `<td data-x="${x}" data-y="${y}" data-color="${gem.color}" style="background-size: ${bgSize}%;"></td>`
-        } else {
-          html += '<td></td>'
-        }
+        html += this.cellHtml(x, y, model)
       }
-      html += '</tr>'
     }
-    html += '</table>'
+    return html
+  }
+
+  cellHtml (x, y, model) {
+    let gem = model.get(x, y)
+    let html = ''
+    let css = `width: ${this.cellSize}px; height: ${this.cellSize}px;`
+    if (gem) {
+      let bgSize = 20 + gem.weight * 10
+      css += ` background-size: ${bgSize}%`
+      html += `<div class="cell" style="${css}" data-x="${x}" data-y="${y}" data-color="${gem.color}"></div>`
+    } else {
+      html += `<div class="cell" style="${css}"></div>`
+    }
     return html
   }
 
