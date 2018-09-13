fde2fedca66464f6458cee544aa0d137c67b43a6
Serge I. Zolotukhin <serge.zolotukhin@gmail.com>
2018-09-13 14:34:45 +0300

    Gems merging method changed.

--- src/classes/Presenter.js
+++ src/classes/Presenter.js
@@ -41,9 +41,26 @@ class Presenter {
     if (gem1 && gem2 && gem1.color !== gem2.color) return false
 
     // Everythin is okay, merge gems
+
+    /**
+     * Когда складываешь камни одного цвета они всегда растут, когда разного цвета,
+     * они уменьшаются на величину того, который задвинули внутрь неподвижного.
+     * Но не меньше стартового размера.
+     */
+
+    if (gem1.color === gem2.color) {
+      gem2.weight += 1
+    } else {
+      gem2.weight -= gem1.weight
+      if (gem2.weight < 1) gem2.weight = 1
+    }
+
+    /**
+     * Old method of merging
     gem2.weight = (gem1.weight <= gem2.weight)
       ? gem2.weight + 1
       : gem1.weight - 1
+    */
 
     this.model.clear(x1, y1)
 
